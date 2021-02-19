f0746562917426f79b2855e158d9ef641cf86bff
Ivan Petrov <asanoryodan@icloud.com>
2021-02-19 17:09:25 +0300

    First version

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Илья Бойков on 19.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
